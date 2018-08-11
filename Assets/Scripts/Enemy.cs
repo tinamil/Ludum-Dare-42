4cd057562a0c20893d45291040ac06f58ce6c030
John Pavlik <john.a.pavlik@gmail.com>
2018-08-11 13:12:47 -0500

    Added training dumy animation and jump attack

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,25 +5,55 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
 
+    GameObject hero;
+    Coroutine runningFade;
+
     // Use this for initialization
     void Start()
     {
-
+        hero = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        transform.LookAt(hero.transform);
     }
 
     private void OnTriggerEnter(Collider other)
     {
         var weapon = other.GetComponent<Weapon>();
-        if(weapon != null)
+        if (weapon != null)
         {
             Debug.Log($"{name} hit by hero's {other.name} and took {weapon.currentDamage} damage");
+            TakeDamage(weapon.currentDamage);
         }
-        
+
+    }
+
+    void TakeDamage(float damage)
+    {
+        GetComponent<Animator>().SetTrigger("OnHit");
+        var textComponent = GetComponentInChildren<TMPro.TMP_Text>(true);
+        textComponent.gameObject.SetActive(true);
+        textComponent.text = damage.ToString();
+        if (runningFade != null)
+        {
+            StopCoroutine(runningFade);
+        }
+        runningFade = StartCoroutine(FadeOut(textComponent));
+    }
+
+
+    IEnumerator FadeOut(TMPro.TMP_Text text, float time = 1f)
+    {
+        var remainingTime = time;
+        while (remainingTime >= 0)
+        {
+            text.color = new Color(text.color.r, text.color.g, text.color.b, Mathf.Lerp(1, 0, (time - remainingTime) / time));
+            remainingTime -= Time.deltaTime;
+            yield return null;
+        }
+        text.gameObject.SetActive(false);
     }
 }
--- Assets/Scripts/HeroController.cs
+++ Assets/Scripts/HeroController.cs
@@ -8,16 +8,19 @@ public class HeroController : MonoBehaviour
 {
     public float runSpeed = 6;
     public float dodgeSpeed = 18;
+    public float jumpAttackSpeed = 12;
+    public float rotationSpeed = 3600;
 
     private CharacterController controller;
     private Animator animator;
-    private Vector3 dodgeDirection;
+    private Vector3 currentDirection;
 
     // Use this for initialization
     void Start()
     {
         controller = GetComponent<CharacterController>();
         animator = GetComponent<Animator>();
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
@@ -28,6 +31,7 @@ public class HeroController : MonoBehaviour
         var isDodgeStart = Input.GetButton("Jump");
         var leftClick = Input.GetMouseButton(0);
         var rightClick = Input.GetMouseButton(1);
+        var mouseLook = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
 
         var direction = transform.forward * vertical + transform.right * horizontal;
 
@@ -38,16 +42,21 @@ public class HeroController : MonoBehaviour
         }
         if (IsInState("Dodge"))
         {
-            Dodge(dodgeDirection);
+            Dodge(currentDirection);
             return;
         }
-        else if (IsInState("DodgeRecovery"))
+        else if (IsInState("DodgeRecovery") || IsInState("Attack Run Recovery"))
         {
             Idle();
             return;
         }
+        else if (IsInState("Attack Run"))
+        {
+            Move(currentDirection, jumpAttackSpeed);
+        }
 
-
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0, transform.rotation.eulerAngles.y + mouseLook.x * rotationSpeed, 0), Time.deltaTime * rotationSpeed);
+  
         animator.SetBool("Attack Right", leftClick);
         animator.SetBool("Attack Down", rightClick);
 
@@ -73,6 +82,7 @@ public class HeroController : MonoBehaviour
         var gravity = Physics.gravity * Time.deltaTime;
         animator.SetFloat("Speed", speed);
         controller.Move(direction.normalized * speed * Time.deltaTime + gravity);
+        currentDirection = direction;
     }
 
     private void Run(Vector3 direction)
@@ -93,7 +103,6 @@ public class HeroController : MonoBehaviour
     private void Dodge(Vector3 direction)
     {
         Move(direction, dodgeSpeed);
-        dodgeDirection = direction.normalized;
     }
 
     private bool IsInState(string stateName)
